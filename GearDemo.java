8387c54e1b8b80980a6b130e946e020c20b0a4e1
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 13:55:45 +0530

    GearAndWheel: Created Wheel as an independent class from Gear Implemented Wheel specific methods like diameter and circumference, and also Gear some methods using these heel methods Also implemented multiple constructors for Gear (as Wheel argument is unnecessary to find ratio)

--- GearDemo.java
+++ GearDemo.java
@@ -1,34 +1,48 @@
 package learnDesign;
 
+class Wheel{
+	private double rim;
+	private double tire;
+	public Wheel(double rim, double tire){
+		this.rim = rim;
+		this.tire = tire;
+	}
+	public double diameter(){
+		return (rim + 2*tire);
+	}
+	public double circumference(){
+		return diameter() * Math.PI;
+	}
+}
+
 class Gear{
 	private int chainring;
 	private int cog;
-	private static class Wheel{
-		double rim;
-		double tire;
-		Wheel(double rim, double tire){
-			this.rim = rim;
-			this.tire = tire;
-		}
-	}
 	private Wheel wheel;
-	public Gear(int chainring, int cog, double rim, double tire){
+	public Gear(int chainring, int cog){
 		this.chainring = chainring;
 		this.cog = cog;
-		this.wheel = new Wheel(rim, tire);
+	}
+
+	public Gear(int chainring, int cog, Wheel wheel){
+		this.chainring = chainring;
+		this.cog = cog;
+		this.wheel = wheel;
 	}
 	public double ratio() {
 		return (double) chainring / cog;
 	}
 	public double gearInches(){
-		return ratio() * (wheel.rim + 2*wheel.tire);
+		return ratio() * wheel.diameter();
 	}
 }
 
 public class GearDemo{
 	public static void main(String[] args){
 		System.out.println("Started...");
-		System.out.println(new Gear(52, 11, 26, 1.5).gearInches());
-		System.out.println(new Gear(52, 11, 24, 1.25).gearInches());
+		System.out.println(new Gear(52, 11).ratio());
+		Wheel wheel = new Wheel(26, 1.5);
+		System.out.println(new Gear(52, 11, wheel).gearInches());
+		System.out.println(wheel.circumference());
 	}
 }
