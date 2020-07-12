3a821cbdc1b932ed8727c6d405b1d5737c97b665
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 13:45:36 +0530

    Gear: Created Wheel struct inside Gear class and used it in gearInches method The wheel may be seperated as a new class in future

--- GearDemo.java
+++ GearDemo.java
@@ -3,19 +3,25 @@ package learnDesign;
 class Gear{
 	private int chainring;
 	private int cog;
-	private double rim;
-	private double tire;
+	private static class Wheel{
+		double rim;
+		double tire;
+		Wheel(double rim, double tire){
+			this.rim = rim;
+			this.tire = tire;
+		}
+	}
+	private Wheel wheel;
 	public Gear(int chainring, int cog, double rim, double tire){
 		this.chainring = chainring;
 		this.cog = cog;
-		this.rim = rim;
-		this.tire = tire;
+		this.wheel = new Wheel(rim, tire);
 	}
 	public double ratio() {
 		return (double) chainring / cog;
 	}
 	public double gearInches(){
-		return ratio() * (rim + 2*tire);
+		return ratio() * (wheel.rim + 2*wheel.tire);
 	}
 }
 
