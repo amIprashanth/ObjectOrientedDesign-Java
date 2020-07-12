bd88f88d6bc4e76758c57eb37a7121a0c2115fbd
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 16:01:16 +0530

    GearAndWheel: Changed the constructor of Gear to accept rim and tire measurements and Constructed Wheel using rima and tire only when necessary This implementation has the drawback that the specific class Wheel is being used A generic Wheel interface can be created to fix this

---
+++ GearAndWheelDemo.java
@@ -0,0 +1,46 @@
+package learnDesign;
+
+class Wheel{
+	private double rim;
+	private double tire;
+	public Wheel(double rim, double tire){
+		this.rim = rim;
+		this.tire = tire;
+	}
+	public double diameter(){
+		return (rim + 2*tire);
+	}
+	public double circumference(){
+		return diameter() * Math.PI;
+	}
+}
+
+class Gear{
+	private int chainring;
+	private int cog;
+	private double rim;
+	private double tire;
+
+	public Gear(int chainring, int cog, double rim, double tire){
+		this.chainring = chainring;
+		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
+	}
+
+	public double ratio() {
+		return (double) chainring / cog;
+	}
+
+	public double gearInches(){
+		Wheel wheel = new Wheel(rim, tire);
+		return ratio() * wheel.diameter();
+	}
+}
+
+public class GearAndWheelDemo{
+	public static void main(String[] args){
+		System.out.println("Started...");
+		System.out.println(new Gear(52, 11, 26, 1.5).gearInches());
+	}
+}
