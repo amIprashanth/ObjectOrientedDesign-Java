7e195fa8be87ca9c8c40dbb9bb1ecf7ba3496675
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 23:24:47 +0530

    Bicycles: MountainBike class inherited from Bicycle class The super constructor and getSpares methods are called to implement the corresponding methods in the subclass Bug: The tireSize being un-overriden is wrongly inherited from the parent (as 23) Additionally, it is not a good inheritence approach, as MountainBike should be more of a sibling to RoadBike (the current Bicycle class) rather than parent.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -4,42 +4,43 @@ import java.util.HashMap;
 
 
 class Bicycle{
-	public String style;
-	public String frontShock;
-	public String rearShock;
-
 	public String size;
 	public String tapeColor;
 
-	private String chain;
-	private String tireSize;
-
-	public Bicycle(String size, String tapeColor, String style, String frontShock, String rearShock){
+	public Bicycle(String size, String tapeColor){
 		this.size = size;
 		this.tapeColor = tapeColor;
-		this.style = style;
-		this.frontShock = frontShock;
-		this.rearShock = rearShock;
 	}
 
 	public HashMap<String, String> getSpares(){
 		HashMap<String, String> newMap = new HashMap<> ();
-		if("road".equals(style)){
-			newMap.put("chain", "10-speed");
-			newMap.put("tireSize", "23");
-			newMap.put("tapeColor", tapeColor);
-		}else{
-			newMap.put("chain", "10-speed");
-			newMap.put("tireSize", "2.1");
-			newMap.put("rearShock", rearShock);
-		}
+		newMap.put("chain", "10-speed");
+		newMap.put("tireSize", "23");
+		newMap.put("tapeColor", tapeColor);
 		return newMap;
 	}
 }
 
+class MountainBike extends Bicycle{
+	private String frontShock;
+	private String rearShock;
+	public MountainBike(String size, String tapeColor, String frontShock, String rearShock){
+		super(size, tapeColor);
+		this.frontShock = frontShock;
+		this.rearShock = rearShock;
+	}
+	
+	public HashMap<String, String> getSpares(){
+		HashMap<String, String> newMap = super.getSpares();
+		newMap.put("rearShock", rearShock);
+		return newMap;
+	}
+
+}
+
 public class BicycleDemo{
 	public static void main(String[] args){
-		Bicycle bike = new Bicycle("S", null, "mountain", "Manitou", "Fox");
+		MountainBike bike = new MountainBike("S", null, "Manitou", "Fox");
 		System.out.println(bike.size);
 		System.out.println(bike.getSpares());
 	}
