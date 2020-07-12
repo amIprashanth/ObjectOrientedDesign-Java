818b3a89c39038cd02281f6dfa2dd4fcf7b652e4
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-13 00:03:13 +0530

    Made Bicycle abstract with defaultTireSize methods for both the subclasses in case it is not provided in the input. This version fixes most of the issues with the previos commits ToDo: A  default getSpares()  method can be added in the parent class; that is, a part of HashMap creation in either subclasses can be abstracted out, and can be merged with the specific fields using super.getSpares() invokation. Note: While making the above enhancement, it has to be remember if a new subclass is created, they may not invoke super() constructor. So, any additions to the Bicycle constructor  are not guaranteed to take place for that subclass.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -3,52 +3,81 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 
-class Bicycle{
-	public String size;
+abstract class Bicycle{
+	protected String size;
+	protected String chain;
+	protected String tireSize;
 
-	public Bicycle(String size){
+	public Bicycle(String size, String chain, String tireSize){
 		this.size = size;
+		this.chain = chain;
+		this.tireSize = tireSize;
 	}
+
+	public String defaultChain(){
+		if(chain!=null) return chain;
+		return "10-speed";
+	}
+
+	abstract public String defaultTireSize();
+	abstract public HashMap<String, String> getSpares();
 }
 
 class RoadBike extends Bicycle{
 	public String tapeColor;
 
-	public RoadBike(String size, String tapeColor){
-		super(size);
+	public RoadBike(String size, String chain, String tireSize, String tapeColor){
+		super(size, chain, tireSize);
 		this.tapeColor = tapeColor;
 	}
 
 	public HashMap<String, String> getSpares(){
 		HashMap<String, String> newMap = new HashMap<> ();
-		newMap.put("chain", "10-speed");
-		newMap.put("tireSize", "23");
+		newMap.put("chain", defaultChain());
+		newMap.put("tireSize", defaultTireSize());
 		newMap.put("tapeColor", tapeColor);
 		return newMap;
+	}
+
+	public String defaultTireSize(){
+		if(tireSize!=null) return tireSize;
+		return "23";
 	}
 }
 
 class MountainBike extends Bicycle{
 	private String frontShock;
 	private String rearShock;
-	public MountainBike(String size, String tapeColor, String frontShock, String rearShock){
-		super(size);
+
+	public MountainBike(String size, String chain, String tireSize, String frontShock, String rearShock){
+		super(size, chain, tireSize);
 		this.frontShock = frontShock;
 		this.rearShock = rearShock;
 	}
 	
 	public HashMap<String, String> getSpares(){
-		HashMap<String, String> newMap = super.getSpares();
-		newMap.put("rearShock", rearShock);
+		HashMap<String, String> newMap = new HashMap<> ();
+		newMap.put("chain", defaultChain());
+		newMap.put("tireSize", defaultTireSize());
+		newMap.put("rearShock", rearShock);		
 		return newMap;
+	}
+
+	public String defaultTireSize(){
+		if(tireSize!=null) return tireSize;
+		return "2.1";
 	}
 
 }
 
 public class BicycleDemo{
 	public static void main(String[] args){
-		MountainBike bike = new MountainBike("S", null, "Manitou", "Fox");
+		RoadBike bike = new RoadBike("M", "30-speed", "100", "red");
 		System.out.println(bike.size);
 		System.out.println(bike.getSpares());
+
+		MountainBike bike1 = new MountainBike("S", null, null, "Manitou", "Fox");
+		System.out.println(bike1.size);
+		System.out.println(bike1.getSpares());
 	}
 }
