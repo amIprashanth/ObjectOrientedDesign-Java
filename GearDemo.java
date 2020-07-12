02da0b73cb0254ed88eb307f2524b2d853e90a6c
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 12:47:28 +0530

    Gear: Created a Gear class with constructor and ratio methods May implement multiple constructors in future

--- GearDemo.java
+++ GearDemo.java
@@ -1,16 +1,21 @@
 package learnDesign;
 
+class Gear{
+	private int chainring;
+	private int cog;
+	public Gear(int chainring, int cog){
+		this.chainring = chainring;
+		this.cog = cog;
+	}
+	public double ratio() {
+		return (double) chainring / cog;
+	}
+}
+
 public class GearDemo{
 	public static void main(String[] args){
 		System.out.println("Started...");
-		int chainring = 52;
-		int cog = 11;
-		double ratio = (double) chainring / cog;
-		System.out.println(ratio);
-
-		chainring = 30;
-		cog = 27;
-		ratio = (double) chainring / cog;
-		System.out.println(ratio);
+		System.out.println(new Gear(52, 11).ratio());
+		System.out.println(new Gear(30, 27).ratio());
 	}
 }
