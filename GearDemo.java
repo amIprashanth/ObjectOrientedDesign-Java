e58ea83cc37e606777221397a9fcb962fb006084
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 12:52:30 +0530

    Gear: Changed the constructor to accept the wheel dimensions and added gearInches method May implement multiple constructors in future

--- GearDemo.java
+++ GearDemo.java
@@ -3,19 +3,26 @@ package learnDesign;
 class Gear{
 	private int chainring;
 	private int cog;
-	public Gear(int chainring, int cog){
+	private double rim;
+	private double tire;
+	public Gear(int chainring, int cog, double rim, double tire){
 		this.chainring = chainring;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 	public double ratio() {
 		return (double) chainring / cog;
+	}
+	public double gearInches(){
+		return ratio() * (rim + 2*tire);
 	}
 }
 
 public class GearDemo{
 	public static void main(String[] args){
 		System.out.println("Started...");
-		System.out.println(new Gear(52, 11).ratio());
-		System.out.println(new Gear(30, 27).ratio());
+		System.out.println(new Gear(52, 11, 26, 1.5).gearInches());
+		System.out.println(new Gear(52, 11, 24, 1.25).gearInches());
 	}
 }
