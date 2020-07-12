5be67b5c8691b19756d5517d26c2ef932b9c54ce
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-12 23:14:39 +0530

    Bicycle: Enhancing the Bicycle class to handle Mountain bike style The type of the bike is stored as a variable rather than creating/inheriting a whole new class Inheriting from Bicycle would be neater as it shares many common features (Future version)

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -2,27 +2,44 @@ package learnDesign;
 import java.util.Arrays;
 import java.util.HashMap;
 
+
 class Bicycle{
+	public String style;
+	public String frontShock;
+	public String rearShock;
+
 	public String size;
 	public String tapeColor;
 
-	public Bicycle(String size, String tapeColor){
+	private String chain;
+	private String tireSize;
+
+	public Bicycle(String size, String tapeColor, String style, String frontShock, String rearShock){
 		this.size = size;
 		this.tapeColor = tapeColor;
+		this.style = style;
+		this.frontShock = frontShock;
+		this.rearShock = rearShock;
 	}
 
 	public HashMap<String, String> getSpares(){
 		HashMap<String, String> newMap = new HashMap<> ();
-		newMap.put("chain", "10-speed");
-		newMap.put("tireSize", "23");
-		newMap.put("tapeColor", tapeColor);
+		if("road".equals(style)){
+			newMap.put("chain", "10-speed");
+			newMap.put("tireSize", "23");
+			newMap.put("tapeColor", tapeColor);
+		}else{
+			newMap.put("chain", "10-speed");
+			newMap.put("tireSize", "2.1");
+			newMap.put("rearShock", rearShock);
+		}
 		return newMap;
 	}
 }
 
 public class BicycleDemo{
 	public static void main(String[] args){
-		Bicycle bike = new Bicycle("M", "red");
+		Bicycle bike = new Bicycle("S", null, "mountain", "Manitou", "Fox");
 		System.out.println(bike.size);
 		System.out.println(bike.getSpares());
 	}
